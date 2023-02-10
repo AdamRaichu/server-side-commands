ab3025ae6843abb9824c326ec20a056f7a2a7938
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-02-10 18:15:19 +0000

    add freeze (slowness only)

--- src/main/java/dev/adamraichu/servercommands/ServerCommands.java
+++ src/main/java/dev/adamraichu/servercommands/ServerCommands.java
@@ -2,14 +2,19 @@ package dev.adamraichu.servercommands;
 
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import static net.minecraft.server.command.CommandManager.literal;
+import static net.minecraft.server.command.CommandManager.argument;
 import net.minecraft.text.*;
-
+import net.minecraft.command.argument.NumberRangeArgumentType;
+import net.minecraft.server.command.ServerCommandSource;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffect;
 
 public class ServerCommands implements ModInitializer {
-
   // This logger is used to write text to the console and the log file.
   // It is considered best practice to use your mod id as the logger's name.
   // That way, it's clear which mod wrote info, warnings, and errors.
@@ -31,5 +36,22 @@ public class ServerCommands implements ModInitializer {
 
               return 1;
             })));
+
+    CommandRegistrationCallback.EVENT
+        .register((dispatcher, registryAccess, environment) -> dispatcher
+            .register(literal("feeeze")
+                .requires(source -> source.hasPermissionLevel(3))
+                .then(argument("player", StringArgumentType.word()))
+                .then(argument("duration", NumberRangeArgumentType.intRange()))
+                .executes(ctx -> freeze(ctx.getSource(), StringArgumentType.getString(ctx, "player"), 30))));
+  }
+
+  private static int freeze(ServerCommandSource source, String playerName, Integer duration) {
+    var player = source.getServer().getPlayerManager().getPlayer(playerName);
+    var slowness = new StatusEffectInstance(StatusEffect.byRawId(2), duration, 4);
+
+    player.addStatusEffect(slowness);
+
+    return Command.SINGLE_SUCCESS;
   }
 }
