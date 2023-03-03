ea6459e26267838296788fc5f1dbd685b6749ae3
= <=>
2023-03-03 03:37:45 +0000

    add readme

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
-# Fabric Example Mod
+# Server Side Commands Mod
 
-## Setup
+This server-side mod adds a couple commands for operators to use.
 
-For setup instructions please see the [fabric wiki page](https://fabricmc.net/wiki/tutorial:setup) that relates to the IDE that you are using.
+## Commands
 
-## License
+Right now the only one is `/freeze`, which traps a player in place.
 
-This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
+### /freeze
+
+```
+/freeze <player> <duration>
+```
+
+Gives `player` the following effects for `duration`.
+
+- Slowness VII
+- Mining Fatigue X
+- Jump Boost -V
+- Blindness I
+- Weakness XX
--- src/main/java/dev/adamraichu/servercommands/ServerCommands.java
+++ src/main/java/dev/adamraichu/servercommands/ServerCommands.java
@@ -12,10 +12,13 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.argument.NumberRangeArgumentType;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 
 import static net.minecraft.server.command.CommandManager.literal;
+
+import java.util.Objects;
+
 import static net.minecraft.server.command.CommandManager.argument;
-import net.minecraft.text.*;
 
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffect;
@@ -35,29 +38,38 @@ public class ServerCommands implements ModInitializer {
     LOGGER.info("Server Commands mod is present.");
 
     CommandRegistrationCallback.EVENT
-        .register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("foo")
-            .executes(context -> {
-              // For versions below 1.19, replace "Text.literal" with "new LiteralText".
-              context.getSource().sendMessage(Text.literal("Called /foo with no arguments"));
-
-              return 1;
-            })));
-
-    CommandRegistrationCallback.EVENT
         .register((dispatcher, registryAccess, environment) -> dispatcher
             .register(literal("freeze")
                 .requires(source -> source.hasPermissionLevel(3))
                 .then(argument("player", StringArgumentType.word())
                     .then(argument("duration", NumberRangeArgumentType.intRange())
-                        .executes(ctx -> freeze(ctx, ctx.getArgument("player", String.class), 30))))));
+                        .executes(ctx -> {
+                          return freeze(ctx, ctx.getArgument("player", String.class), 30);
+                        })))));
   }
 
   private static int freeze(CommandContext<ServerCommandSource> context, String playerName, Integer duration)
       throws CommandSyntaxException {
     var player = context.getSource().getServer().getPlayerManager().getPlayer(playerName);
-    var slowness = new StatusEffectInstance(StatusEffect.byRawId(2), duration, 4);
+
+    if (Objects.isNull(player)) {
+      context.getSource().sendError(Text.literal("Player does not exist"));
+      return 0;
+    }
+
+    var slowness = new StatusEffectInstance(StatusEffect.byRawId(2), duration, 6);
+    var mining_fatigue = new StatusEffectInstance(StatusEffect.byRawId(4), duration, 9);
+    var jump_boost = new StatusEffectInstance(StatusEffect.byRawId(8), duration, -5);
+    var blindness = new StatusEffectInstance(StatusEffect.byRawId(15), duration, 0);
+    var weakness = new StatusEffectInstance(StatusEffect.byRawId(18), duration, 19);
 
     player.addStatusEffect(slowness);
+    player.addStatusEffect(mining_fatigue);
+    player.addStatusEffect(jump_boost);
+    player.addStatusEffect(blindness);
+    player.addStatusEffect(weakness);
+
+    player.sendMessage(Text.literal(context.getSource().getName() + " froze you."));
 
     return Command.SINGLE_SUCCESS;
   }
