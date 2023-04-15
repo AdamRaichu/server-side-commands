ef2e8e5890f335d5a445ccb6735c1103166d40a3
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-04-15 15:36:53 +0000

    initial working config

--- src/main/java/dev/adamraichu/servercommands/ServerCommands.java
+++ src/main/java/dev/adamraichu/servercommands/ServerCommands.java
@@ -3,6 +3,10 @@ package dev.adamraichu.servercommands;
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Properties;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +15,7 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
@@ -24,24 +29,38 @@ public class ServerCommands implements ModInitializer {
   // That way, it's clear which mod wrote info, warnings, and errors.
   public static final Logger LOGGER = LoggerFactory.getLogger("servercommands");
 
+  private Properties CONFIG;
+
   @Override
   public void onInitialize() {
     // This code runs as soon as Minecraft is in a mod-load-ready state.
     // However, some things (like resources) may still be uninitialized.
     // Proceed with mild caution.
 
+    CONFIG = new ServerCommandsConfig(FabricLoader.getInstance().getConfigDir().resolve(
+        "servercommands.properties").toFile())
+        .getConfig();
+
     LOGGER.info("Server Side Commands mod is present.");
 
-    CommandRegistrationCallback.EVENT
-        .register((dispatcher, registryAccess, environment) -> dispatcher
-            .register(literal("freeze")
-                .requires(source -> source.hasPermissionLevel(2))
-                .then(argument("player", EntityArgumentType.player())
-                    .then(argument("duration", IntegerArgumentType.integer(1))
-                        .executes(ctx -> {
-                          return freezePlayer(ctx.getSource(), EntityArgumentType.getPlayer(ctx, "player"),
-                              ctx.getArgument("duration", Integer.class) * 20);
-                        })))));
+    try {
+
+      Integer requiredPermissionLevel = toNumber(CONFIG.getProperty("cmds.freeze.permissionLevel", "2")).intValue();
+
+      CommandRegistrationCallback.EVENT
+          .register((dispatcher, registryAccess, environment) -> dispatcher
+              .register(literal("freeze")
+                  .requires((source) -> source.hasPermissionLevel(requiredPermissionLevel))
+                  .then(argument("player", EntityArgumentType.player())
+                      .then(argument("duration", IntegerArgumentType.integer(1))
+                          .executes(ctx -> {
+                            return freezePlayer(ctx.getSource(), EntityArgumentType.getPlayer(ctx, "player"),
+                                ctx.getArgument("duration", Integer.class) * 20);
+                          })))));
+      LOGGER.info("Commands registered");
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
   }
 
   private static int freezePlayer(ServerCommandSource source, ServerPlayerEntity player, Integer duration) {
@@ -59,8 +78,12 @@ public class ServerCommands implements ModInitializer {
 
     player.sendMessage(Text.literal(source.getName() + " froze you."));
 
-    LOGGER.info(source.getName() + " froze " + player.getName().toString() + ".");
+    LOGGER.info(source.getName() + " froze " + player.getName().getString() + ".");
 
     return Command.SINGLE_SUCCESS;
+  }
+
+  private Number toNumber(String string) throws ParseException {
+    return NumberFormat.getInstance().parse(string);
   }
 }
